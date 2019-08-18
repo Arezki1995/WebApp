f76f1e1137945760ea9341c6e7d889a72d3993c3
Arezki1995 <ait.mouheb.arezki@gmail.com>
2019-08-18 22:56:02 +0200

    Designing global website layout with multiple pages

--- application/server.js
+++ application/server.js
@@ -26,27 +26,50 @@ const HOST = '0.0.0.0';
 const app = express();
 
 app.set('views',path.join(__dirname, 'views'));
+
+app.use('/assets', express.static('assets'));
+//app.use('/products', express.static('products'));
+
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
 
-  
-  
   mysqlConnection.query('SELECT * FROM Tasks;', (err, rows, fields)=>{
       if(!err){
 
-        var nb =rows.length
-        res.render('index', {nb_tasks:nb, tasks:rows});  
+        res.render('home', {tasks:rows});  
       }else{
         console.log('error getting tasks');
       }
 
-
   });
-  
-  
-
 });
+
+
+
+app.get('/About', (req, res) => {
+        res.render('about');  
+});
+
+
+
+app.get('/Products', (req, res) => {
+  mysqlConnection.query('SELECT * FROM Products;', (err, rows, fields)=>{
+    if(!err){
+
+      res.render('products', {products:rows});  
+    }else{
+      console.log('error getting products');
+    }
+
+});  
+});
+
+
+app.get('/Contact', (req, res) => {
+  res.render('contact');  
+});
+
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
